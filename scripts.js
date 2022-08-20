ce026186d818f1fb9e2d23b427f4f5c87cdd15ca
Michael <michaeljsouza9@gmail.com>
2022-08-19 23:08:32 -0700

    Margin color issue resolved
    Added for loop so that background color only changes on
    generated grid

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 // Javascript! //
 
 function createGrid() {
-    for(let i=0; i<16; i++) {
+    const size = prompt("How large would you like your grid?", "16");
+
+    const pixel = 500/size;
+
+    for(let i=0; i<size; i++) {
         let column = document.createElement("div");
-        for(let j=0; j<16; j++) { 
+        for(let j=0; j<size; j++) { 
             let cell = document.createElement("div");
-            cell.style.width = "31.25px";
-            cell.style.height = "31.25px";
+            cell.style.width = `${pixel}px`;
+            cell.style.height = `${pixel}px`;
             cell.style.background = "red";
             cell.style.display = "inline-block";
             cell.addEventListener("mouseover", over);
@@ -21,7 +25,9 @@ function createGrid() {
 document.getElementById("grid").addEventListener("mouseover", over);
 
 function over() {
-    this.style.backgroundColor = "white";
+    if(this.style.backgroundColor === "red"){
+    this.style.backgroundColor = "black";
+    }
 }
 
 function reset() {
